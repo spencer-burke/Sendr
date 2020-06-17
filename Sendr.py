a7ef0ae36b45386586f27b0718321ed0145e5d92
Spencer Burke <info.spencer.burke@gmail.com>
2020-06-16 21:54:39 -0700

    Working on a very primitive version of the client

--- Sendr.py
+++ Sendr.py
@@ -2,19 +2,31 @@ import asyncio
 import socket
 import os
 
-import asyncio
+COM_PORT = 8888
+
+async def transfer_data(reader, writer, data):
+     writer.write(data.encode())
+     await writer.drain()
+     writer.write_eof()
 
 async def client(message):
-    reader, writer = await asyncio.open_connection(
-        '127.0.0.1', 8888)
+    reader, writer = await asyncio.open_connection('127.0.0.1', 8888)
 
-    writer.write(message.encode())
-
+    await transfer_data(reader, writer, message)
     data = await reader.read()
-    print(f'Received: {data.decode()!r}')
 
-    print('Close the connection')
+    if data.decode() == "ack":
+        print("command recieved")
+
     writer.close()
-
+ 
 asyncio.run(client('store'))
+
+listener_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listener_sock.bind(('127.0.0.1', 8889))
+listener_sock.listen()
+conn, addr = listener_sock.accept()
+
+# code can be run after this
+# currently working on getting an asynchronous transport client side(or just sucessfully sending data
 
