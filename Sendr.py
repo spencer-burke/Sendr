a4eacac79086fb628e1dadc126e9f8888958ecc1
Spencer Burke <info.spencer.burke@gmail.com>
2020-06-20 16:23:50 -0700

    Successfully can send and receive files; refactors needed

--- Sendr.py
+++ Sendr.py
@@ -33,18 +33,20 @@ async def send_command(command):
 
 async def send_file_name():
     listener_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listener_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     listener_sock.bind(('127.0.0.1', 8889))
     listener_sock.listen()
     conn, addr = listener_sock.accept()
 
     n_reader, n_writer = await asyncio.open_connection(sock=conn)
 
-    await transfer_data(n_reader, n_writer, "example_text_file.txt")
+    await transfer_data_encoded(n_reader, n_writer, "example_text_file.txt")
 
     listener_sock.close()
 
 async def send_file_data():
     listener_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listener_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     listener_sock.bind(('127.0.0.1', 8889))
     listener_sock.listen()
     conn, addr = listener_sock.accept()
@@ -53,9 +55,9 @@ async def send_file_data():
 
     with open("example_text_file.txt", 'rb') as file_reader:
         data = file_reader.read()
-        writer.write(data)
-        await writer.drain()
-        writer.write_eof()
+        n_writer.write(data)
+        await n_writer.drain()
+        n_writer.write_eof()
 
     listener_sock.close()
 
@@ -72,30 +74,26 @@ async def send_file():
         n_reader, n_writer = await asyncio.open_connection(sock=conn)
         
         # send the name of the file
-        await transfer_data(n_reader, n_writer, "example_text_file.txt")
+        await transfer_data_encoded(n_reader, n_writer, "example_text_file.txt") 
         # get an acknowledgement
         response = await n_reader.read() 
-
-        # accept the server a second time for the file data  
-        conn, addr = writer_sock.accept()
+        #n_writer.close()
+        #await n_writer.wait_closed()
         
-        # open asynchronous connection from raw connection
-        d_reader, d_writer = await asyncio.open_connection(sock=conn) 
+        d_reader, d_writer = await asyncio.open_connection(sock=conn)
+    
+        file_writer = open("example_text_file.txt", 'rb')
+        data = file_writer.read()
+        print(data)
+        d_writer.write(data)
+        await d_writer.drain()
+        d_writer.close()
+        print("sucessfully sent file")
 
-        # write the file data over the network
-        with open("example_text_file.txt", 'rb') as file_reader:
-            data = file_reader.read()
-            writer.write(data)
-            await writer.drain()
-            writer.write_eof()
-
-        # get a response
-        response = await d_reader.read()
-
-#asyncio.run(send_command('store'))
-#asyncio.run(send_file_name())
-#asyncio.run(send_file_data())
-asyncio.run(send_file())
+asyncio.run(send_command('store'))
+asyncio.run(send_file_name())
+asyncio.run(send_file_data())
+#asyncio.run(send_file())
 # currently working on a better way to recieve data being the socket keeps getting used repeatedly
 
 @click.group()
