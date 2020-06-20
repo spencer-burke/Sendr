806ecedcf5c5fcc2998ddf86bdb723364c3d1a3f
Spencer Burke <info.spencer.burke@gmail.com>
2020-06-19 18:39:32 -0700

    Adding a prototype routine for a better method to receive data using one 'bind' call

--- Sendr.py
+++ Sendr.py
@@ -59,14 +59,43 @@ async def send_file_data():
 
     listener_sock.close()
 
-def send_file():
+async def send_file():
     await send_command('store')
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as listener:
-        pass 
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as writer_sock:
+        writer_sock.bind(('127.0.0.1', DATA_PORT))
+        # listen for the server connecting for the file data 
+        writer_sock.listen()
+        # accept server connection
+        conn, addr = writer_sock.accept()
+        
+        # open asynchronous acception
+        n_reader, n_writer = await asyncio.open_connection(sock=conn)
+        
+        # send the name of the file
+        await transfer_data(n_reader, n_writer, "example_text_file.txt")
+        # get an acknowledgement
+        response = await n_reader.read() 
+
+        # accept the server a second time for the file data  
+        conn, addr = writer_sock.accept()
+        
+        # open asynchronous connection from raw connection
+        d_reader, d_writer = await asyncio.open_connection(sock=conn) 
+
+        # write the file data over the network
+        with open("example_text_file.txt", 'rb') as file_reader:
+            data = file_reader.read()
+            writer.write(data)
+            await writer.drain()
+            writer.write_eof()
+
+        # get a response
+        response = await d_reader.read()
+
 #asyncio.run(send_command('store'))
 #asyncio.run(send_file_name())
 #asyncio.run(send_file_data())
-
+asyncio.run(send_file())
 # currently working on a better way to recieve data being the socket keeps getting used repeatedly
 
 @click.group()
